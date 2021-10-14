09af9ac3b60b1d3fa1e72f8c4ae90e9cbec64b81
rafacalvo <rafael.calvo.gonzalez@gmail.com>
2021-10-13 21:12:16 -0400

    Final commit before deploy

---
+++ main.js
@@ -0,0 +1,8 @@
+// Functions to open and close the sidenav go here:
+function openNav() {
+  document.getElementById('sidenav').style.left = '0';
+}
+
+function closeNav() {
+  document.getElementById('sidenav').style.left = '-100%';
+}
